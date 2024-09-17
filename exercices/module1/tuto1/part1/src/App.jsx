12fef0a22b2602cd3fe3840fcff1d7236ca8309d
RobertAilenei <robert.ailenei@student.vinci.be>
2024-09-17 15:08:51 +0200

    finished tuto 1

---
+++ exercices/module1/tuto1/part1/src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
+  )
+}
+
+export default App
