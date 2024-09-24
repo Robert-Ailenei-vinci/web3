b9f18d2f4938730dfc6a1cea59f98e7c2c942310
Robert <robert.ailenei@student.vinci.be>
2024-09-24 13:00:10 +0200

    finish tuto 2

---
+++ module2/tuto/e-vinci web3-exercises main tutorials-start-state-starter/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import "./Header.css";
+
+const Header = ({ title, version }) => {
+  const [menuPrinted, setMenuPrinted] = useState(false);
+
+  return (
+    <header onClick={() => setMenuPrinted(!menuPrinted)}>
+      <h1 className="animate__animated animate__bounce">
+        {menuPrinted ? `${title}... and rarely do we hate it!` : title}
+      </h1>
+      <h4>Version: {version}</h4>
+    </header>
+  );
+};
+
+
+
+export default Header;
