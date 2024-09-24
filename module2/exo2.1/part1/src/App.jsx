44d2f164229b1dd26b0925ac79114833e9b3a6cd
Robert <robert.ailenei@student.vinci.be>
2024-09-24 13:45:57 +0200

    new:ex2.1-events

--- module2/exo2.1/part1/src/App.jsx
+++ module2/exo2.1/part1/src/App.jsx
@@ -16,12 +16,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-       <Click_counter />
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <Click_counter masterMessage="You are a master in the art of clicking !" messageHover="Please click me now!"/>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- module2/exo2.1/part1/src/ClickCounter.jsx
+++ module2/exo2.1/part1/src/ClickCounter.jsx
@@ -1,17 +1,35 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 
+const ClickCounter = ({ masterMessage, messageHover }) => {
+  const [count, setCount] = useState(0);
+  const [hoverMessage, setHoverMessage] = useState("");
 
-const Click_counter = () => {
-    const [count, setCount] = useState(0)
-    
-    return(
-        <button onClick={() => setCount((count) => count + 1)}>
+  // Fonction appelée quand la souris entre dans le bouton
+  const handleMouseEnter = () => {
+    setHoverMessage(messageHover);
+  };
+
+  // Fonction appelée quand la souris quitte le bouton
+  const handleMouseLeave = () => {
+    setHoverMessage("");
+  };
+
+  return (
+    <div className="card">
+      <button
+        onClick={() => setCount((count) => count + 1)}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         count is {count}
       </button>
-    )
+      {hoverMessage && <p>{hoverMessage}</p>}
+      
+      {/* Affichage du message si le nombre de clics est supérieur ou égal à 10 */}
+      {count >= 10 && <p>{masterMessage}</p>}
+    </div>
+  );
+};
 
-
-}
-
-
-export default Click_counter;
+export default ClickCounter;
