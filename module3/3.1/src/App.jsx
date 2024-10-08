f3dd8f293b414b539159f13649ee4e7381806e10
RobertAilenei <robert.ailenei@student.vinci.be>
2024-10-08 14:06:01 +0200

    finish ex 1.7

--- module3/3.1/src/App.jsx
+++ module3/3.1/src/App.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
  
 import { useState } from 'react'
-
 const Titl = () => {
     return (
         <h1>give feedback</h1>
@@ -28,23 +27,43 @@ const StatsText = (props) => {
     )
 }
 
-
+const StatsAll = (props) => {
+    const {Good, Neutral, Bad, Somme} = props;
+    const all=Good+Neutral+Bad;
+    const pourcent= (Good/all)*100 
+    const moyenne= Somme/all;
+    return(
+        <h3>all: {all} <br />
+        average : {moyenne} <br />
+        Positive feedback : {pourcent}  %      
+        </h3>
+    )
+}
 
 const App = () => {
+    
+    
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [somme, setSomme] = useState(0);
 
   const handleGood = () => {
     const updatedGood = good+1;
+    const updatedSomme = somme+1;
     setGood(updatedGood);
+    setSomme(updatedSomme);
   }
   const handleNeutral = () => {
     const updatedNeutral = neutral+1;
+    const updatedSomme = somme;
+    setSomme(updatedSomme);
     setNeutral(updatedNeutral);
   }
   const handleBad = () => {
+    const updatedSomme = somme-1;
+    setSomme(updatedSomme);
     const updatedBad = bad+1;
     setBad(updatedBad);
   }
@@ -61,6 +80,7 @@ const App = () => {
         <StatsText text="Good" value={good}/>
         <StatsText text="Neutral" value={neutral}/>
         <StatsText text="Bad" value={bad}/>
+        <StatsAll  Good={good}  Neutral={neutral} Bad = {bad} Somme={somme}/>
     </div>
   )
 }
